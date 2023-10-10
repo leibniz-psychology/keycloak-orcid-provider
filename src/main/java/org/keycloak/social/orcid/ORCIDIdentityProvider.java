99cce1b57a8ed0d642569448d59f321ffeaa8b46
Manuel Biertz <mab@leibniz-psychology.org>
2023-10-10 15:09:21 +0200

    REFACTOR to transition from Java EE to Jakarta EE.

--- src/main/java/org/keycloak/social/orcid/ORCIDIdentityProvider.java
+++ src/main/java/org/keycloak/social/orcid/ORCIDIdentityProvider.java
@@ -43,11 +43,11 @@ import org.keycloak.util.JsonSerialization;
 import org.keycloak.services.ErrorResponseException;
 import org.keycloak.representations.JsonWebToken;
 
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.UriBuilder;
 import java.io.IOException;
 
 /**
